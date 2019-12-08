3673445dc7f11e92f2e8fe503fd0377c0a5eedd5
diogoandrade1999 <diogo.andrade@ua.pt>
2019-12-08 17:31:39 +0000

    serializer

--- GymBit/main/views.py
+++ GymBit/main/views.py
@@ -1,7 +1,9 @@
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from main.serializers import WorkModelSerializer, ExercisesModelSerializer, UserModelSerializer
+from main.models import UserModel, WorkModel, ExercisesModel
 
 from django.shortcuts import render
 
@@ -14,25 +16,20 @@ def trainees_view(request):
     return render(request, "trainees.html")
 
 
-def trainee_info_view(request):
-    return render(request, "trainee_profile.html")
-
-
 def charts_view(request):
     return render(request, "charts.html")
 
 
+@api_view(['GET'])
 def profile_view(request):
-    return render(request, "profile.html")
+    #id = int(request.GET['id'])
+    try:
+        user = UserModel.objects.get(name='Diogo')
+    except ObjectDoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = UserModelSerializer(user)
+    return render(request, "profile.html", {'user': serializer.data})
 
 
 def tables_view(request):
     return render(request, "tables.html")
-
-
-def login_view(request):
-    return render(request, "login.html")
-
-
-def register_view(request):
-    return render(request, "register.html")
