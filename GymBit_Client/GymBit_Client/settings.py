eee1b297f0c2fdbd7b2763876547e37b72dcbcce
diogoandrade1999 <diogo.andrade@ua.pt>
2019-12-21 17:16:10 +0000

    db in vm

--- GymBit_Client/GymBit_Client/settings.py
+++ GymBit_Client/GymBit_Client/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-LOGIN_REDIRECT_URL = 'trainees.html'
+LOGIN_REDIRECT_URL = 'index.html'
 LOGIN_URL = 'login.html'
 
 API_URL = 'http://localhost:9000/'
